4371769e7d3d7716ccaf6c089a2cfdcc20882f63
Someesvaar <someesvaar.v@gmail.com>
2026-04-13 15:40:02 +0530

    Add imports for ChromeOptions and WebDriverWait

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,6 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+
 public class App {
     public static void main(String[] args) throws InterruptedException {
 
